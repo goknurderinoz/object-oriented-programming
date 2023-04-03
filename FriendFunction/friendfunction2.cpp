9b83991e2e132454d70c2c0f28297f4e07e0f235
goknurderinoz <goknur.derinoz@gmail.com>
2023-04-03 17:32:04 +0300

    (+)Add friend function 2 example

---
+++ FriendFunction/friendfunction2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class B;
+class A{
+    private:
+        int num1;
+    public:
+        void getValueA();
+        friend void Add(A,B);
+    
+};
+void A::getValueA(){
+    cout<<"Enter first Number:";
+    cin>>num1;
+}
+
+class B{
+    private:
+        int num2;
+    public:
+        void getValueB();
+        friend void Add(A,B);
+};
+
+void B::getValueB(){
+    cout<<"Enter second Number: ";
+    cin>>num2;
+}
+
+void Add(A obj1, B obj2){
+    cout<<"The result is: "<<obj1.num1 + obj2.num2;
+}
+
+int main() {
+    A obj1;
+    B obj2;
+    obj1.getValueA();
+    obj2.getValueB();
+    Add(obj1, obj2);
+    return 0;
+}
